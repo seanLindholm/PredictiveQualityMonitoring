b8ee1ec19aeda51ef0aba311382cbbda629f3e0d
Sean <seanclindholm@gmail.com>
2021-01-17 15:35:39 +0100

    CreatedProject

---
+++ Src/Constants.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import IsolationForest as isoF
+from sklearn.cluster import KMeans as km
+from sklearn.decomposition import PCA
+
+
+# ------ Function for data manupulation ------- #
+
+def normalized(dt):
+    return (dt - dt.min(0)) / dt.ptp(0)
+    
+
+def standardize_data(dt):
+    pass
+
+def outlierRemoval_mask(dt):
+    # This outlier detector makes use of the Isolated tree, from
+    # sklearn library
+    iso = isoF(contamination=0.1)
+    yhat = iso.fit_predict(dt)
+    return yhat != -1
+
+def kMean(dt,num_clus,seed=0):
+    # This function utilises sklearn's implementation
+    # of the kMeans algortihmn
+    return km(n_clusters=num_clus, random_state=seed).fit(dt)
+
+def lowestComponancePCA(dt,min_comp,explain_var):
+    for i in range(min_comp,dt.shape[1]+1):
+        pca = PCA(n_components=i)
+        pca.fit(dt)
+        if(sum(pca.explained_variance_ratio_) >= explain_var):
+            print(sum(pca.explained_variance_ratio_))
+            return pca
+    return pca
+# -------------------------------------------- #
+
+
+# ----- Function for numpy pretty print ------ #
+def print_matrix(M,header=[],float_precision=3):
+    if (header != []):
+        pass                                                                                                                                                                                                                                            
+    for row in M:
+        print("|",end='')
+        for v in row:
+            print("{0:>7.{1}f} ".format(v,float_precision),end='')
+        print("|") 
+  
+
+def print_array(A,float_precision=3):
+    print("|",end='')
+    for v in A:
+        print("{0:>7.{1}f} ".format(v,float_precision),end='')
+    print("|")   
+ 
+
+# -------------------------------------------- #
+
+
+#The path to the data used
+data_path = "C:\\Users\\swang\\Desktop\\Sean\\Speciale\\pyEnv\\Data"
+approved_file_name = data_path + "\\Approved_w_glu_3000rows_15_1_2021.csv"
+failed_file_name = data_path + "\\Failed_w_glu_15_1_2021.csv"
+
+
+#Reading in data and removing the onwanted columns
+approved_data = pd.read_csv(approved_file_name,sep=r'\s*,\s*',engine='python')
+col_mapping_dict = {c[1]:c[0] for c in enumerate(approved_data.columns)}
+approved_data = approved_data.to_numpy()
+failed_data = pd.read_csv(failed_file_name,sep=r'\s*,\s*',engine='python').to_numpy()
+
+#For now we remove the 'Tid efter start [timer]' and the 'Tid in GLU' since these values are due to human handling 
+#and not a measure of the product
+approved_data = np.delete(approved_data,[col_mapping_dict['Tid efter start [timer]'],col_mapping_dict['Tid i min Glu']],1)
+failed_data = np.delete(failed_data,[col_mapping_dict['Tid efter start [timer]'],col_mapping_dict['Tid i min Glu']],1)
+
+# We here also avoid the class column since this is just binary good/bad 
+f_input,f_output = failed_data[:,:3],failed_data[:,4:]
+
+
+# We now remove outliers in the output - the reason that we do it in the output is that the outlier is not 
+# Tied to the input but rather the the machine worked probaly when measuring. This can also be tied to 
+# the resolution pack not working proberly, or being to old.
+mask = outlierRemoval_mask(f_output)
+f_input,f_output = f_input[mask,:], f_output[mask,:]
+
+
+# Same for the approved data, alongside a reduction such that we got half and half 
+a_input,a_output = approved_data[:,:3],approved_data[:,4:]
+mask = outlierRemoval_mask(a_output)
+a_input,a_output = a_input[mask,:], a_output[mask,:]
+
+
+# Now build the f_output from a reduced set (PCA) after outlier removal
+# Find the lowest number of components that still explain 95% variance
+pca = lowestComponancePCA(dt = normalized(f_output),min_comp=2,explain_var = 0.95)
+f_output_pca = pca.fit_transform(normalized(f_output))
+
+
+pca_2 = PCA(n_components = 2)
+pca_2.fit(normalized(f_output))
+f_pca2 = pca_2.fit_transform(normalized(f_output))
+
+
---
+++ Src/Kmeans.py
@@ -0,0 +1,87 @@
+
+from Constants import *
+from collections import Counter
+import matplotlib.pyplot as plt
+
+def FindMaxEuclidDistFromSet(L_dt,center_matrix):
+    ret = {}
+    center_indx = 0
+    for dt in L_dt:
+        max_dist = 0
+        for row in dt:
+            dist = np.linalg.norm(row-center_matrix[center_indx,:])
+            if (max_dist < dist):
+                max_dist = dist
+        ret[center_indx] = max_dist
+        center_indx+=1
+    print("\nThe maximum euclidian distance of an element to the closets centroid")
+    print(ret)
+    return ret
+
+
+def kMeansWithLabels(dt,n_clusters,seed):
+    L_dt = []
+    KMs = kMean(dt,n_clusters,seed)
+    cluster_label = np.transpose(np.array([KMs.labels_]))
+    f_out = np.append(dt,cluster_label,axis=1)
+    # Print the number of points in the centers along the centers themself
+    print(f"The cluster centroids for {n_clusters} clusters:")
+    print_matrix(KMs.cluster_centers_)
+    print("\nNumber of elements in each cluster:")
+    print(Counter(KMs.labels_))
+
+    # devide the output into the labels using masks
+    for i in range(n_clusters):
+        mask = f_out[:,-1] == i
+        L_dt.append(f_out[mask,:-1])
+
+    # Return the matrix and the Matrix of matrixes where it has been divided 
+    return f_out,L_dt,KMs.cluster_centers_
+
+def testAndPrintUpToNClusters(dt,n_clusters,seed):
+    for i in range(1,n_clusters+1):
+        _,L_dt,center_matrix = kMeansWithLabels(dt,i,seed)
+        _ = FindMaxEuclidDistFromSet(L_dt,center_matrix)
+        print()
+
+def pcaClosterPlot(clusters):
+    if clusters > 0:
+        f_o,_,center_matrix = kMeansWithLabels(f_pca2,clusters,1337)
+        c_mask = np.zeros(f_o.shape[0])
+        for i in range(clusters):
+            mask = f_o[:,-1] == i
+            c_mask += mask*(i+1)
+
+        plt.scatter(f_o[:,0],f_o[:,1],c=c_mask,alpha=0.4,marker='.')
+        plt.scatter(center_matrix[:,0],center_matrix[:,1],c='black',marker='+',alpha=1,s=100)
+        plt.title(f"marked clusters with {clusters} number of clusters.")
+    else:
+       
+        plt.scatter(f_pca2[:,0],f_pca2[:,1],marker='.')
+        plt.title("data with 2 principal components")
+
+
+def main():
+    print("Use of Kmeans clustering after removal of outliers with NO reduction of dimention:\n")
+    testAndPrintUpToNClusters(normalized(f_output),f_output.shape[1],1337)
+    print("\n\n\nUse of Kmeans clustering after removal of outliers with PCA reduction:\n")
+    testAndPrintUpToNClusters(f_output_pca,f_output_pca.shape[1],1337)
+  
+    # Show plot of a 2D - pca clustering
+    plt.figure('No clusters')
+    pcaClosterPlot(0)
+    plt.figure('Two clusters')
+    pcaClosterPlot(2)
+    plt.figure('Three clusters')
+    pcaClosterPlot(3)
+    plt.figure('Four clusters')
+    pcaClosterPlot(4)
+
+    plt.show(block=False)
+    input("Press Enter to close the figures...")
+    plt.close('all')
+
+    # Maybe use the DBSCAN to find density clusters
+if __name__ == "__main__":
+    main()
+   
