aa247ee66c19ac32303df1e7608d74658c6489dc
Sean <seanclindholm@gmail.com>
2021-01-21 20:46:35 +0100

    More clean up and ready dea script

--- Src/Helper.py
+++ Src/Helper.py
@@ -5,37 +5,6 @@ from sklearn.cluster import KMeans as km
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 
-# ------ Function for data manupulation ------- #
-
-def normalized(dt):
-    return (dt - dt.min(0)) / dt.ptp(0)
-    
-
-def standardized(dt):
-    scale = preprocessing.StandardScaler().fit(dt)
-    return scale.fit_transform(dt)
-
-def outlierRemoval_mask(dt):
-    # This outlier detector makes use of the Isolated tree, from
-    # sklearn library
-    iso = isoF(contamination=0.1)
-    yhat = iso.fit_predict(dt)
-    return yhat != -1
-
-def kMean(dt,num_clus,seed=0):
-    # This function utilises sklearn's implementation
-    # of the kMeans algortihmn
-    return km(n_clusters=num_clus, random_state=seed).fit(dt)
-
-def lowestComponancePCA(dt,min_comp,explain_var):
-    for i in range(min_comp,dt.shape[1]+1):
-        pca = PCA(n_components=i)
-        pca.fit(dt)
-        if(sum(pca.explained_variance_ratio_) >= explain_var):
-            print(sum(pca.explained_variance_ratio_))
-            return pca
-    return pca
-# -------------------------------------------- #
 
 
 # ----- Function for numpy pretty print ------ #
@@ -54,9 +23,170 @@ def print_array(A,float_precision=3):
     for v in A:
         print("{0:>7.{1}f} ".format(v,float_precision),end='')
     print("|")   
- 
-
 # -------------------------------------------- #
+
+
+#The path to the data used
+data_path = "..\\Data"
+approved_file_name = data_path + "\\Approved_w_glu.csv"
+failed_file_name = data_path + "\\Failed_w_glu.csv"
+approved_file_Transform_name = data_path + "\\Approved_w_glu_Transform.csv"
+failed_file_Transform_name = data_path + "\\Failed_w_glu_Transform.csv"
+
+
+# pca_2 = PCA(n_components = 2)
+# pca_2.fit(standardized(f_output))
+# print("------------------------------------------")
+# print("Standardized - PCA: ",end='');print_array(pca_2.explained_variance_ratio_)
+# f_pca2_std = pca_2.fit_transform(standardized(f_output))
+# pca_2.fit(normalized(f_output))
+# print("------------------------------------------")
+# print("Normalized - PCA: ",end='');print_array(pca_2.explained_variance_ratio_)
+# print("------------------------------------------")
+# f_pca2_norm = pca_2.fit_transform(normalized(f_output))
+
+
+class Data_handler:
+    def __init__(self,file_path_csv):
+        """
+            file_path: a direct or indirect path the to file with the whole name, including
+                       the .csv extension\n
+            self.dt: The datatabel to be manipulated with\n
+            self.colMap: A mapping between column names and the indcies. Can be seen by printing the class
+                         or by setting help=True in the function removeColumns\n
+        """
+        self.__path = file_path_csv
+        dt_t = pd.read_csv(file_path_csv,sep=r'\s*,\s*',engine='python')
+        self.__normalized = False
+        self.colMap = np.array([c[1] for c in enumerate(dt_t.columns)])
+        self.dt = dt_t.to_numpy()
+    
+    def str_support(self):
+        pass
+
+    def setPath(self,name_path):
+        self.__path = name_path
+
+    def __str__(self):
+        return f"Datatabel has {self.dt.shape[0]} samples with {self.dt.shape[1]} columns\nThe column names that can be used as keys for the column mapping are as follows:\n{ self.colMap }"
+
+    def __repr__(self):
+        return f"Data analysis from path {self.__path}"
+
+    def removeColumns(self,col_name_list,h=False):
+        """
+            Used to alter the datatabel by removing unwanted 
+            columns. Use the option h=True, if you want to see what
+            the column names of the datatable is. Setting help till true
+            will not alter the datatabel.
+        """
+        if h:
+            print("Available column name in the datatable:\n")
+            for name in self.colMap:
+                print(f"{name}")
+        else:
+            indexis = [np.where(self.colMap == name) for name in col_name_list]
+            self.colMap = np.delete(self.colMap,indexis,0)
+            self.dt = np.delete(self.dt,indexis,1)
+    
+    
+    def normalized(self):
+        """
+            Normalizes the datatable in the range (0-1) 
+            making use of the min value and peek-to-peek range (max-min)
+            and the minimum
+        """
+        self.org_dt_min =self.dt.min(0)
+        self.org_dt_ptp = self.dt.ptp(0)
+        self.dt = (self.dt - self.org_dt_min) / self.org_dt_ptp
+        self.__normalized = True
+    
+    def un_normalized(self):
+        """
+            This function restore the the normalized data tabel into
+            the un-normalized version.
+        """
+        if self.__normalized:
+            self.dt = self.dt*self.org_dt_ptp+self.org_dt_min
+            self.__normalized = False
+        
+    def splitData(self,split_indx):
+        """
+            This is used to split input from output. Only use this function after you have done 
+            all the data manipulation you want.\n
+            self.dt_in: Ranged from [0:split_indx[\n
+            self.dt_out:Ranged from [split_indx:end]\n
+        """
+        self.dt_in = self.dt[:,:split_indx]
+        self.dt_out = self.dt[:,split_indx:]
+
+    # ------ Function for data manupulation ------- #
+    
+    def standardized(self):
+        scale = preprocessing.StandardScaler().fit(self.dt)
+        return scale.fit_transform(self.dt)
+
+
+    def outlierRemoval(self,contamination=0.1):
+        """ 
+            This outlier detector makes use of the Isolated tree method, from
+            the sklearn library.
+        """
+        iso = isoF(contamination=contamination)
+        yhat = iso.fit_predict(self.dt)
+        mask = yhat != -1
+        self.dt = self.dt[mask,:]
+        return 
+
+    def kMean(self,dt,num_clus,seed=0):
+        """ 
+         This function utilises sklearn's implementation
+         of the kMeans algortihmn
+        """
+        return km(n_clusters=num_clus, random_state=seed).fit(dt)
+    
+    def lowestComponancePCA(self,min_comp,explain_var,output_only = True):
+        """
+            This function builds finds the lowest number of Principal Components
+            that is equal to or above the specified explain_variance (range 0-1)
+        """
+        if (output_only):
+            if self.dt_out == []: 
+                print("You haven't split the data in input-output yet!")
+                return 
+            else:
+                for i in range(min_comp,self.dt_out.shape[1]+1):
+                    pca = PCA(n_components=i)
+                    pca.fit(self.dt_out)
+                    if(sum(pca.explained_variance_ratio_) >= explain_var):
+                        return pca
+                return pca
+        else:
+            for i in range(min_comp,self.dt.shape[1]+1):
+                pca = PCA(n_components=i)
+                pca.fit(self.dt)
+                if(sum(pca.explained_variance_ratio_) >= explain_var):
+                    return pca
+            return pca
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # TODO:
@@ -69,64 +199,15 @@ def print_array(A,float_precision=3):
   *Perfomance evaluation (DEA - Data envelopment analysis) know sample is measured with x param each has value, the higher the worse
 
   *Distance -> performance index for sample. DEA gives unique number for each sample. From x -> 1 score
-  *pyDEA - > library
+  *pyDEA - > library: http://people.brunel.ac.uk/~mastjjb/jeb/or/dea.html
   *Be able to show the result is robust to the method, by showing simluar results from different methods
-  *Understading of the underlying method. 
+  *Understading of the underlying method - exam. 
+
 
 """
 
-#The path to the data used
-data_path = "..\\Data"
-approved_file_name = data_path + "\\Approved_w_glu.csv"
-failed_file_name = data_path + "\\Failed_w_glu.csv"
-approved_file_Transform_name = data_path + "\\Approved_w_glu_Transform.csv"
-failed_file_Transform_name = data_path + "\\Failed_w_glu_Transform.csv"
-
-
-#Reading in data and removing the onwanted columns
-approved_data = pd.read_csv(approved_file_name,sep=r'\s*,\s*',engine='python')
-col_mapping_dict = {c[1]:c[0] for c in enumerate(approved_data.columns)}
-approved_data = approved_data.to_numpy()
-failed_data = pd.read_csv(failed_file_name,sep=r'\s*,\s*',engine='python').to_numpy()
-
-#For now we remove the 'Tid efter start [timer]' and the 'Tid in GLU' since these values are due to human handling 
-#and not a measure of the product
-approved_data = np.delete(approved_data,[col_mapping_dict['Tid i min Glu']],1)
-failed_data = np.delete(failed_data,[col_mapping_dict['Tid i min Glu']],1)
-
-# We here also avoid the class column since this is just binary good/bad 
-f_input,f_output = failed_data[:,:3],failed_data[:,4:]
-
-
-# We now remove outliers in the output - the reason that we do it in the output is that the outlier is not 
-# Tied to the input but rather the the machine worked probaly when measuring. This can also be tied to 
-# the resolution pack not working proberly, or being to old.
-mask = outlierRemoval_mask(f_output)
-f_input,f_output = f_input[mask,:], f_output[mask,:]
 
 
 
-# Same for the approved data, alongside a reduction such that we got half and half 
-a_input,a_output = approved_data[:,:3],approved_data[:,4:]
-mask = outlierRemoval_mask(a_output)
-a_input,a_output = a_input[mask,:], a_output[mask,:]
-
-
-# Now build the f_output from a reduced set (PCA) after outlier removal
-# Find the lowest number of components that still explain 95% variance
-pca = lowestComponancePCA(dt = normalized(f_output),min_comp=2,explain_var = 0.95)
-f_output_pca = pca.fit_transform(normalized(f_output))
-
-
-pca_2 = PCA(n_components = 2)
-pca_2.fit(standardized(f_output))
-print("------------------------------------------")
-print("Standardized - PCA: ",end='');print_array(pca_2.explained_variance_ratio_)
-f_pca2_std = pca_2.fit_transform(standardized(f_output))
-pca_2.fit(normalized(f_output))
-print("------------------------------------------")
-print("Normalized - PCA: ",end='');print_array(pca_2.explained_variance_ratio_)
-print("------------------------------------------")
-f_pca2_norm = pca_2.fit_transform(normalized(f_output))
 
 
--- Src/Kmeans.py
+++ Src/Kmeans.py
@@ -21,7 +21,7 @@ def FindMaxEuclidDistFromSet(L_dt,center_matrix):
 
 def kMeansWithLabels(dt,n_clusters,seed):
     L_dt = []
-    KMs = kMean(dt,n_clusters,seed)
+    KMs = Data_handler.kMean(Data_handler,dt,n_clusters,seed)
     cluster_label = np.transpose(np.array([KMs.labels_]))
     f_out = np.append(dt,cluster_label,axis=1)
     # Print the number of points in the centers along the centers themself
@@ -39,7 +39,8 @@ def kMeansWithLabels(dt,n_clusters,seed):
     return f_out,L_dt,KMs.cluster_centers_
 
 def testAndPrintUpToNClusters(dt,n_clusters,seed):
-    for i in range(1,n_clusters+1):
+    if n_clusters < 2: n_clusters = 2; 
+    for i in range(2,n_clusters+1):
         _,L_dt,center_matrix = kMeansWithLabels(dt,i,seed)
         _ = FindMaxEuclidDistFromSet(L_dt,center_matrix)
         print()
@@ -61,35 +62,80 @@ def pcaClosterPlot(dt,clusters):
         plt.title("data with 2 principal components")
 
 
-def main():
-    print("Use of Kmeans clustering after removal of outliers with NO reduction of dimention - Normalized:\n")
-    testAndPrintUpToNClusters(normalized(f_output),4,1337)#f_output.shape[1],1337)
-    print("Use of Kmeans clustering after removal of outliers with NO reduction of dimention - Standardized:\n")
-    testAndPrintUpToNClusters(standardized(f_output),4,1337)#f_output.shape[1],1337)
-    print("\n\n\nUse of Kmeans clustering after removal of outliers with PCA reduction:\n")
-    testAndPrintUpToNClusters(f_output_pca,4,1337)
-  
-    # Show plot of a 2D - pca clustering
-    plt.figure('No clusters - norm')
-    pcaClosterPlot(f_pca2_norm,0)
-    plt.figure('Two clusters - norm')
-    pcaClosterPlot(f_pca2_norm,2)
-    plt.figure('Three clusters - norm')
-    pcaClosterPlot(f_pca2_norm,3)
+def printOutput(dh_list):
+    for dh in dh_list:
+        print()
+        print(dh.__repr__())
+        print()
+        pca_red = dh.lowestComponancePCA(min_comp=2,explain_var = 0.95,output_only = True).fit_transform(dh.dt_out)
+        print("Use of Kmeans clustering after removal of outliers with NO reduction of dimention - Normalized:\n")
+        testAndPrintUpToNClusters(dh.dt_out,3,0)#f_output.shape[1],1337)
+        #print("Use of Kmeans clusterng after removal of outliers with NO reduction of dimention - Standardized:\n")
+        #testAndPrintUpToNClusters(standardized(f_output),4,1337)#f_output.shape[1],1337)
+        print("\n\n\nUse of Kmeans clustering after removal of outliers with PCA reduction:\n")
+        testAndPrintUpToNClusters(pca_red,3,0)
+    
 
-    # Show plot of a 2D - pca clustering
-    plt.figure('No clusters - std')
-    pcaClosterPlot(f_pca2_std,0)
-    plt.figure('Two clusters - std')
-    pcaClosterPlot(f_pca2_std,2)
-    plt.figure('Three clusters - std')
-    pcaClosterPlot(f_pca2_std,3)
+    # print ("Use of kMeans clustering on Transformed dataset - normalized")
+    
+    # # Show plot of a 2D - pca clustering
+    # plt.figure('No clusters - norm')
+    # pcaClosterPlot(f_pca2_norm,0)
+    # plt.figure('Two clusters - norm')
+    # pcaClosterPlot(f_pca2_norm,2)
+    # plt.figure('Three clusters - norm')
+    # pcaClosterPlot(f_pca2_norm,3)
+
+
+    # # Show plot of a 2D - pca clustering
+    # plt.figure('No clusters - std')
+    # pcaClosterPlot(f_pca2_std,0)
+    # plt.figure('Two clusters - std')
+    # pcaClosterPlot(f_pca2_std,2)
+    # plt.figure('Three clusters - std')
+    # pcaClosterPlot(f_pca2_std,3)
 
     plt.show(block=False)
     input("Press Enter to close the figures...")
     plt.close('all')
 
-    # Maybe use the DBSCAN to find density clusters
+    
+
+
+def main():
+    dh_l = [Data_handler(approved_file_name),Data_handler(failed_file_name),Data_handler(failed_file_name)]
+    dh_l_transform = [Data_handler(approved_file_Transform_name),Data_handler(failed_file_Transform_name),Data_handler(failed_file_Transform_name)]
+    
+    for dh in dh_l:
+        dh.removeColumns(['Class','Tid i min Glu'])
+        
+    for dh in dh_l_transform:
+        dh.removeColumns(['Class'])
+
+    for elm in dh_l_transform:
+        dh_l.append(elm)
+    
+    Ran_app_sel = np.random.permutation(np.arange(dh_l[0].dt.shape[0]))[:dh_l[1].dt.shape[0]]
+    Ran_app_sel_trans = np.random.permutation(np.arange(dh_l[3].dt.shape[0]))[:dh_l[4].dt.shape[0]]
+    dt_mixed = np.append(dh_l[1].dt,dh_l[0].dt[Ran_app_sel,:],axis=0)
+    dt_mixed_trans = np.append(dh_l[4].dt,dh_l[3].dt[Ran_app_sel_trans,:],axis=0)
+
+    dh_l[2].dt = dt_mixed
+    dh_l[2].setPath("mixed tabel with approved and failed")
+    dh_l[5].dt = dt_mixed_trans
+    dh_l[5].setPath("mixed tabel with approved and failed - Transformed")
+
+
+    for dh in dh_l:
+        dh.outlierRemoval()
+        df = dh.dt
+        dh.normalized()
+        dh.splitData(3)
+
+    printOutput(dh_l)
+
+
+    
 if __name__ == "__main__":
     main()
    
